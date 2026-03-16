bacd25524f5c45831c57fcedad113a99b428933a
Thinakaran B <baskerthina52@gmail.com>
2026-03-16 11:19:20 +0530

    Delete basic.js

--- basic.js
+++
@@ -1,9 +0,0 @@
-function greeting(){
-    let name=document.getElementById('name').value;
-    if(name==""){
-        alert("Please enter your name...");
-    }
-    if(name!=""){
-        document.writeln("Hi! "+name);
-    }
-}
